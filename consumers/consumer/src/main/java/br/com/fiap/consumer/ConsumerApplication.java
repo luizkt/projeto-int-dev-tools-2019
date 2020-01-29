666d8d8bb2e26c19f58a6d8f808b70f8f703e2af
loganmb <logan.baruch@fatec.sp.gov.br>
2020-01-29 20:20:58 -0300

    consumer java

---
+++ consumers/consumer/src/main/java/br/com/fiap/consumer/ConsumerApplication.java
@@ -0,0 +1,35 @@
+package br.com.fiap.consumer;
+
+import br.com.fiap.consumer.kafka.dto.KafkaMessage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class ConsumerApplication {
+
+	KafkaMessage msg;
+
+	@GetMapping("/")
+	public KafkaMessage consumeMsgs(){
+		return msg;
+	}
+
+	@KafkaListener(groupId = "my-app", topics = "entrada", containerFactory = "kafkaListenerContainerFactory")
+	public KafkaMessage getMsgFromTopic(KafkaMessage data){
+		msg = data;
+		return msg;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
