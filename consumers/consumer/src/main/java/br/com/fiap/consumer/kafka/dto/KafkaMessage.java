6a0576f542f4d433c404cda2bd3072ec7eb8155e
loganmb <logan.baruch@fatec.sp.gov.br>
2020-01-28 23:36:20 -0300

    alteração da classe dto consumer java

--- consumers/consumer/src/main/java/br/com/fiap/consumer/kafka/dto/KafkaMessage.java
+++ consumers/consumer/src/main/java/br/com/fiap/consumer/kafka/dto/KafkaMessage.java
@@ -3,13 +3,76 @@ package br.com.fiap.consumer.kafka.dto;
 
 public class KafkaMessage {
 
-    private String attribute;
+    private String mesReferencia;
+    private String mesCompetencia;
+    private String uf;
+    private String codMunicipio;
+    private String nomeMunicipio;
+    private String nisFavorecido;
+    private String nomeFavorecido;
+    private double valorParcela;
 
-    public String getAttribute() {
-        return attribute;
+    public String getMesReferencia() {
+        return mesReferencia;
     }
 
-    public void setAttribute(String attribute) {
-        this.attribute = attribute;
+    public void setMesReferencia(String mesReferencia) {
+        this.mesReferencia = mesReferencia;
+    }
+
+    public String getMesCompetencia() {
+        return mesCompetencia;
+    }
+
+    public void setMesCompetencia(String mesCompetencia) {
+        this.mesCompetencia = mesCompetencia;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCodMunicipio() {
+        return codMunicipio;
+    }
+
+    public void setCodMunicipio(String codMunicipio) {
+        this.codMunicipio = codMunicipio;
+    }
+
+    public String getNomeMunicipio() {
+        return nomeMunicipio;
+    }
+
+    public void setNomeMunicipio(String nomeMunicipio) {
+        this.nomeMunicipio = nomeMunicipio;
+    }
+
+    public String getNisFavorecido() {
+        return nisFavorecido;
+    }
+
+    public void setNisFavorecido(String nisFavorecido) {
+        this.nisFavorecido = nisFavorecido;
+    }
+
+    public String getNomeFavorecido() {
+        return nomeFavorecido;
+    }
+
+    public void setNomeFavorecido(String nomeFavorecido) {
+        this.nomeFavorecido = nomeFavorecido;
+    }
+
+    public double getValorParcela() {
+        return valorParcela;
+    }
+
+    public void setValorParcela(double valorParcela) {
+        this.valorParcela = valorParcela;
     }
 }
