81b07715d65ca5789f0f90ea110578b4ccca1ae6
luizkt <keese.luiz@gmail.com>
2020-01-29 22:45:06 -0300

    Arrumado problema de perda de mensagens para o Kafka

--- producers/nodejs-producer/index.js
+++ producers/nodejs-producer/index.js
@@ -1,20 +1,42 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+var fs = require('fs');
+var parse = require('csv-parse');
 
 var producer = require('./kafka/producer.js');
 
-var dados = [];
-index = 0;
+// name of the csv file
+const inputFile = './../../files/bolsaFamiliaTestes.csv';
 
-fs.createReadStream('./../../files/bolsaFamiliaTestes.csv', 'utf8')
-    .pipe(csv({ separator: ';' }))
-    .on('data', async (row) => {
+let RecordArray;
 
-        row["VALOR PARCELA"] = parseFloat(row["VALOR PARCELA"].replace(',', '.'));
+const parser = parse({delimiter: ';'}, function (err, data) {
+    RecordArray = data;
+    // when all data are available,then process the first one
+    // note: array element at index 0 contains the row of headers that we should skip
+    handleCurrentRecord(1);
+});
 
-        dados.push(row);
-        await producer.run(JSON.stringify(row)).catch(e => console.error(`[example/producer] ${e.message}`, e));
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-    })
+// read the inputFile, feed the contents to the parser
+fs.createReadStream(inputFile).pipe(parser);
+
+// handle the current record
+function handleCurrentRecord(currentRecord) {
+    var line = RecordArray[currentRecord];
+
+    line[7] = parseFloat(line[7].replace(',', '.'));
+
+    var record = {
+        "mesReferencia": line[0]
+        ,"mesCompetencia": line[1]
+        ,"uf": line[2]
+        ,"codigoMunicipioSiafi": line[3]
+        ,"nomeMunicipio": line[4]
+        ,"nisFavorecido": line[5]
+        ,"nomeFavorecido": line[6]
+        ,"valorParcela": line[7]
+    };
+    console.log(JSON.stringify(record));
+    // produce message to Kafka
+    producer.produceKafkaMessage(record);
+
+    setTimeout(handleCurrentRecord.bind(null, currentRecord + 1), 1);
+}
