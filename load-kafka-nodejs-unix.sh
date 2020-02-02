7943d7e50605c22a6c38b3369e7bd79b3a9b53b1
Luiz Keese Tabacow <31867028+luizkt@users.noreply.github.com>
2020-02-02 14:32:28 -0300

    Update load-kafka-nodejs-unix.sh

--- load-kafka-nodejs-unix.sh
+++ load-kafka-nodejs-unix.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 cd ./producers/nodejs-producer
+
+ECHO "#####################################"
+ECHO "#                                   #"
+ECHO "#   Resolving nodejs dependencies   #"
+ECHO "#                                   #"
+ECHO "#####################################"
+
 npm install
+
+ECHO "#############################"
+ECHO "#                           #"
+ECHO "#       Starting load       #"
+ECHO "#                           #"
+ECHO "#############################"
+
 node index.js
+
+ECHO "############################"
+ECHO "#                          #"
+ECHO "#       Load finished      #"
+ECHO "#                          #"
+ECHO "############################"
